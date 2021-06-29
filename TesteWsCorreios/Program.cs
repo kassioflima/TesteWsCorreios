1113461727b05544dfbba9c62c5e18efb61fe897
kassiofernandeslima <72845184+kassiofernandeslima@users.noreply.github.com>
2021-06-28 23:31:05 -0300

    Add project files.

---
+++ TesteWsCorreios/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TesteWsCorreios
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu CEP");
+            string cep = Console.ReadLine();
+            await ConsultarCep(cep);
+        }
+
+        public static async Task ConsultarCep(string cep)
+        {
+            var correios = new Correios.AtendeClienteClient();
+            var consulta = await correios.consultaCEPAsync(cep);
+
+            Console.Clear();
+            Console.WriteLine($"Bairro: { consulta.@return.bairro}");
+            Console.WriteLine($"Cep: { consulta.@return.cep}");
+            Console.WriteLine($"Cidade: { consulta.@return.cidade}");
+            Console.WriteLine($"Complemento 2: { consulta.@return.complemento2}");
+            Console.WriteLine($"Endereço: { consulta.@return.end}");
+            Console.WriteLine($"Uf: { consulta.@return.uf}");
+            Console.ReadKey();
+        }
+    }
+}
